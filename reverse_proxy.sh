398f9d4afa8a489e767110d9afbbbe93989d09da
cortez <89983388+cortez24rus@users.noreply.github.com>
2025-01-28 12:49:30 +0700

    1.4.1a

--- reverse_proxy.sh
+++ reverse_proxy.sh
@@ -5,11 +5,11 @@
 ### Global values
 ###################################
 export DEBIAN_FRONTEND=noninteractive
-VERSION_MANAGER=1.4.1
+VERSION_MANAGER=1.4.1a
 DEFAULT_FLAGS="/usr/local/reverse_proxy/default.conf"
 PATH_DB="/etc/x-ui/x-ui.db"
-SCRIPT_URL="https://raw.githubusercontent.com/cortez24rus/xui-reverse-proxy/refs/heads/main/reverse_proxy.sh"
-DB_SCRIPT_URL="https://raw.githubusercontent.com/cortez24rus/xui-reverse-proxy/refs/heads/main/database/x-ui.db"
+SCRIPT_URL="https://raw.githubusercontent.com/cortez24rus/xui-reverse-proxy/refs/heads/dev/reverse_proxy.sh"
+DB_SCRIPT_URL="https://raw.githubusercontent.com/cortez24rus/xui-reverse-proxy/refs/heads/dev/database/x-ui.db"
 
 ###################################
 ### Initialization and Declarations
